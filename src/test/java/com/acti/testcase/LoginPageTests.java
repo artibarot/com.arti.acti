acd05bc95e4e71109dbcf2c696c6f1eb05d70857
User <User@VLAD.lan>
2023-05-11 17:23:58 -0500

    Made a change to the Src/Test/java-LoginpageTests

--- src/test/java/com/acti/testcase/LoginPageTests.java
+++ src/test/java/com/acti/testcase/LoginPageTests.java
@@ -1,17 +1,17 @@
 package com.acti.testcase;
 
-import org.testng.Assert;
-//import org.testng.annotations.AfterMethod;
-//import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import com.acti.base.DriverScript;
+import com.acti.pages.LoginPage;
 import org.testng.annotations.Test;
-//import com.acti.base.DriverScript;
-//import com.acti.pages.LoginPage;
+import org.testng.Assert;
 
 //make global variable to use in entire proj
 
 
-
-public class LoginPageTests extends BaseTest{
+public class LoginPageTests extends BaseTest  
+{
 	@Test   //1st test case -Login page title
 	public void testLoginPageTitle() throws InterruptedException
 	{ 
@@ -22,37 +22,38 @@ public class LoginPageTests extends BaseTest{
 		Assert.assertEquals(actual, expected);
 		Thread.sleep(3000);
 	}
-	@Test     //2nd test case- ActiImage displayed
+	@Test   (enabled = true)  //2nd test case- ActiImage displayed
 	public void testActiImgDisplayed() throws InterruptedException
 	{
 		boolean flag = lp.VerifyActiImg();
 		Assert.assertTrue(flag);
 		Thread.sleep(3000);
 	}
-	
-	@Test     //3rd test case- Forgot password displayed
+	@Test  (enabled = true)   //3rd test case- Forgot password displayed
 	public void testForgotPasswordedDisplayed() throws InterruptedException
 	{
 		boolean flag = lp.verifyForgotpasswordLink();
 		Assert.assertTrue(flag);
 		Thread.sleep(3000);
 	}
-	@Test     //4th test case- Log in with Admin
+	@Test  (enabled = true)   //4th test case- Log in with Admin
 	public void testLoginFunction() throws InterruptedException
 	{
 		lp.enterUsername("admin");
 		lp.enterPassword("manager");
-	    lp.ClickLoginButton();
-    Thread.sleep(3000);
+		lp.ClickLoginButton();
+        Thread.sleep(3000);
 	}
-	@Test  //(enabled = True) //5th test case- error message
+	@Test  (enabled = true) //5th test case- error message
 	public void testLoginInvalidCredientials() throws InterruptedException
 	{
+		lp.enterUsername("admin");
 		lp.enterPassword("admin");
 		lp.ClickLoginButton();
-		String errmsg = lp.getErrMsgText();
 		Thread.sleep(3000);
+		String errmsg = lp.getErrMsgText();
 		System.out.println(errmsg);
-		Assert.assertTrue(errmsg.contains("username or password is invalid"));
+		Assert.assertTrue(errmsg.contains("Username or Password is invalid"));		
 	}
 }
+
