cefe5b4fde8168eb1b1e1112c28f00308495fc43
User <User@VLAD.lan>
2023-05-16 23:02:44 -0500

    Added report class method and Beforesuite

--- src/test/java/com/acti/testcase/BaseTest.java
+++ src/test/java/com/acti/testcase/BaseTest.java
@@ -2,16 +2,34 @@ package com.acti.testcase;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
 import com.acti.base.DriverScript;
 import com.acti.pages.EnterPage;
 import com.acti.pages.LoginPage;
 import com.acti.pages.TaskPage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class BaseTest extends DriverScript {
-	//make global variable to use in entire proj
+//make global variable to use in entire 
+public static ExtentHtmlReporter extent;
+public static ExtentReports report;
+public static ExtentTest Logger;
+
 LoginPage lp;
 EnterPage ep;
 TaskPage tp ;
+
+@BeforeSuite
+public void reportSetup()
+{
+	extent= new ExtentHtmlReporter("./reports/actiindex.html");
+	report = new ExtentReports();
+	report.attachReporter(extent);
+}
+
 	
 	@BeforeMethod
 	public void preTest() 
@@ -24,6 +42,7 @@ TaskPage tp ;
 	@AfterMethod
 	public void tearDown()
 	{
+		report.flush();
 		quitDriver();
 	}
 
